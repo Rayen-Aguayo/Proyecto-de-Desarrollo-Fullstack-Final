4a52eb6f82dcee22ea08ef23dd1f9e4e0b1afe3c
javiera-faundez <ja.faundezb@duocuc.cl>
2026-06-18 16:56:01 -0400

    feat: update gateway y pom medico

--- Consulta Médica y Dental/ms-medico/ms-medico/src/main/java/com/example/ms_medico/MsMedicoApplication.java
+++ Consulta Médica y Dental/ms-medico/ms-medico/src/main/java/com/example/ms_medico/MsMedicoApplication.java
@@ -1,9 +1,13 @@
 package com.example.ms_medico;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ImportAutoConfiguration(exclude = {
+    org.springdoc.core.configuration.SpringDocHateoasConfiguration.class
+})
 public class MsMedicoApplication {
 
 	public static void main(String[] args) {
